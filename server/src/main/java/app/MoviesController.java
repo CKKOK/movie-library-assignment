9457968cc744e32ed31f0090371fa7f4badd5d52
CKKOK <kok.chee.kean@outlook.com>
2018-06-24 12:09:02 +0800

    back to angular separated

--- server/src/main/java/app/MoviesController.java
+++ server/src/main/java/app/MoviesController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +15,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("/movies")
 public class MoviesController {
     private final MovieDatabase movieDatabase;
     
